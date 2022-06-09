1f90bc5afc22a55fa5227d395883f4938409c40f
Rahmat Zulkifli <onepiece3985@gmail.com>
2022-06-09 22:15:36 +0800

    [07] Rahmat Zulkifli 152235865100-419

--- promise.js
+++ promise.js
@@ -1,8 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = async (yangDihitung)=>{
+  try{
+    let hasil=0;
+    let arr1=await promiseTheaterIXX();
+    let arr2=await promiseTheaterVGC();
+    let IXX=await arr1.filter(element=>element.hasil==yangDihitung);
+    let VGC=await arr2.filter(element=>element.hasil==yangDihitung);    
+    return IXX.length+VGC.length;
+  }catch(err){
+    return err;
+  }
+};
 module.exports = {
   promiseOutput,
 };
